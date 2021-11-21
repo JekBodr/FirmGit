742a510ec8d2ca3fb06c14e320ed87f72053b48f
JekBodr <jekbodr@gmail.com>
2021-11-21 23:49:12 +0200

    Add information in main

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Демонстрация работы программы");
+
         Client model = new Client();
         ClientView view = new ClientView(model);
         ClientController controller = new ClientController(model, view);
